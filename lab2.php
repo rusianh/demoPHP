deaf683686f1ca61f7ac5ddf09b1a326ed50981e
VietAnhVu <vva1994@gmail.com>
2021-01-12 19:49:09 +0700

    them

---
+++ lab2.php
@@ -0,0 +1,39 @@
+<?php
+//for each question below, try to find a built-in php function.
+//if there is none, implement a function to solve the question.
+
+
+//split string "this is a sentence" by spaces. Echo each word to the screen
+
+
+//replace all vowels a,e,i,o,u in the following sentence with '-'
+//"Hello World Of PHP"
+
+
+//print the following string in revesed order
+//"Hello World Of PHP"
+
+
+//find the last occurence of 'o' in the string
+//"Hello World Of PHP"
+
+
+//given an array ["orange", "banana", "apple", "raspberry"] 
+//print out the last element of the array and remove it from the array.
+
+
+//given an array ["orange", "banana", "apple", "raspberry"] 
+//check whether "apple", "lemon" are in the array or not
+
+
+//given an array of 11 random number
+//sort the array in ascending order
+
+
+//Do the question in different\different.php
+
+
+//Do the question in multiplication\multiplication.php
+
+
+?>
---
+++ multiplication/multiplication.php
@@ -0,0 +1,25 @@
+<!-- 
+QUESTION:
+
+provide PHP code with 2-3 loops + if..elseif...else + simple calculation
+*to generate a multiplication table*
+your result should look the same as multiplication_end.png
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Challenge: using loops</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Multiplication Table</h1>
+<table>
+    <?php
+        //YOUR CODE GOES HERE
+    ?>
+</table>
+</body>
+</html>
