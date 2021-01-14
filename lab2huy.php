34a8a758000e248ad95a94eb38c2df0685a19326
VietAnhVu <vva1994@gmail.com>
2021-01-14 20:13:26 +0700

    hoan thanh bai 1 multication

---
+++ lab2huy.php
@@ -0,0 +1,47 @@
+<!-- 
+QUESTION:
+
+provide PHP code with 2-3 loops + if..elseif...else + simple calculation
+*to generate a multiplication table*
+your result should look the same as multiplication_end.png
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Challenge: using loops</title>
+    <link href="style1.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Multiplication Table</h1>
+<table>
+    <?php
+        for($i = 0; $i < 13; $i++){
+            if($i == 0){
+                echo "<tr>";
+            for($j = 0; $j < 13; $j++){
+                if($j == 0){
+                    echo "<th></th>";
+                } else {
+                    echo "<th>" . $j . "</th>";
+                }
+            }
+            echo "</tr>";
+            } else {
+                echo "<tr>";
+            for($j = 0; $j < 13; $j++){
+                if($j == 0){
+                    echo "<th>" . $i . "</th>";
+                } else {
+                    echo "<td>" . $j*$i . "</td>";
+                }
+            }
+            echo "</tr>";
+            }
+        }
+    ?>
+</table>
+</body>
+</html>
--- multiplication/multiplication.php
+++ multiplication/multiplication.php
@@ -18,14 +18,26 @@ your result should look the same as multiplication_end.png
 <h1>Multiplication Table</h1>
 <table>
     <?php
-        //YOUR CODE GOES HERE
         for($i=0; $i<13; $i++){
             if($i==0){
                 echo "<tr>";
                 for($j=0;$j<13; $j++){
-                    echo "th"
+                    if($j==0){
+                        echo "<th></th>";
+                    } else {
+                        echo "<th>". $j ."</th>";
+                    }
+                } echo "</tr>";
+            } else {
+                echo "<tr>";
+                for ($j = 0; $j <13; $j++){
+                    if ($j==0) {
+                        echo "<th>". $i ."</th>";
+                    } else {
+                        echo "<td>". $i*$j ."</td>";
+                    }
                 }
-            }
+            } echo "</tr>";
         }
     ?>
 </table>
