b899aafd49c9b32e3155868d68b09d0cef6c4bc7
VietAnhVu <vva1994@gmail.com>
2021-01-23 19:50:17 +0700

    lam bai lab3 login

---
+++ lab03lvttulam/login.php
@@ -0,0 +1,26 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form>
+            <h2>Đăng nhập</h2>
+            <div>
+                <input type="text" name="" class="" placeholder="Điền tên đăng nhập" required >
+            </div>
+            <div>
+                <input type="text" name="" class="" placeholder="Điền mật khẩu" >
+            <div>
+                <button type="submit" class="" name="" >Truy Cập</button>
+            </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
