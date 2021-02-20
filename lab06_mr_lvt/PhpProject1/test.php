6d523d3d933339faffd75f55f40aeb074f9855fa
VietAnhVu <vva1994@gmail.com>
2021-02-20 23:18:34 +0700

    add

---
+++ lab06_mr_lvt/PhpProject1/test.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ví dụ Bootstrap --- dammio.com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+ 
+<div class="container">
+  <h2>Các phong cách nút nhấn --- dammio.com</h2>
+  <button type="button" class="btn">Cơ bản</button>
+  <button type="button" class="btn btn-primary">Chính</button>
+  <button type="button" class="btn btn-secondary">Phụ</button>
+  <button type="button" class="btn btn-success">Thành công</button>
+  <button type="button" class="btn btn-info">Thông tin</button>
+  <button type="button" class="btn btn-warning">Cảnh báo</button>
+  <button type="button" class="btn btn-danger">Nguy hiểm</button>
+  <button type="button" class="btn btn-dark">Tối</button>
+  <button type="button" class="btn btn-light">Sáng</button>
+  <button type="button" class="btn btn-link">Liên kết</button>      
+</div>
+ 
+</body>
+</html>
---
+++ lab06_practise/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>System Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .login-form{
+            width: 340px;
+            margin: 50px auto;
+            font-size: 15px;
+        }
+    </style>
+
+</head>
+<body>
+    <div class="login-form">
+        <form>
+            <h2>Đăng nhập</h2>
+            <div>
+                <input type="text" name="fname" class="" placeholder="Tên đăng nhập" required>
+                <input>
+                <button>Đăng nhập</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
